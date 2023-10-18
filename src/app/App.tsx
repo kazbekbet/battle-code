3623e4dfc356ad2cc75a07fb4b6197be5bed71d0
Artyco <artyco.w.w@gmail.com>
2023-10-18 15:55:25 +0300

    routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,43 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+import CreateTaskPage from "../pages/CreateTaskPage";
+import GamePage from "../pages/GamePage";
+import TasksPage from "../pages/TasksPage";
+import AuthPage from "../pages/AuthPage";
+import HomePage from "../pages/HomePage";
+import { Protected } from "./routing/Protected";
+
 export default function App() {
   return (
     <>
-      <p>EntryPoint2</p>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/auth">Auth</Link>
+        <Link to="/tasks">Tasks</Link>
+        <Link to="/create-task">Create task</Link>
+        <Link to="/game">Create task</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route
+          path="/tasks"
+          element={
+            <Protected>
+              <TasksPage />
+            </Protected>
+          }
+        />
+        <Route
+          path="/create-task"
+          element={
+            <Protected>
+              <CreateTaskPage />
+            </Protected>
+          }
+        />
+        <Route path="/game" element={<GamePage />} />
+      </Routes>
     </>
   );
 }
